c1cb352bc4a6d02fa9c2d82814ffb78b0baa6522
farah-habarnih <90200659+farah-habarnih@users.noreply.github.com>
2021-10-31 11:08:33 +0200

    Add files via upload

---
+++ Source Code/contact.js
@@ -0,0 +1,23 @@
+let togglebtn = document.querySelector('.toggle-btn')
+let navbar = document.querySelector('.navbar')
+
+togglebtn.addEventListener('click', ()=>{
+    navbar.classList.toggle('mobile-nav')
+} )
+
+function myFunction() {
+var element = document.body;
+element.classList.toggle("dark-mode");
+}
+
+
+
+let fname= document.getElementById("fname");
+let userform=document.getElementById("form-validate");
+let last= document.getElementById("lname");
+
+userform.addEventListener("submit", function validate(e){
+    e.preventDefault();
+    localStorage.setItem("first name",JSON.stringify(fname.value));
+    localStorage.setItem("last name",JSON.stringify(last.value));
+})
