e519003ae2ec269f5f87d573a516e48105d3b5ea
Farah Habarnih <90200659+farah-habarnih@users.noreply.github.com>
2022-01-18 10:46:13 +0200

    Add files via upload

--- Source Code/contact.js
+++ Source Code/contact.js
@@ -1,23 +1,30 @@
-let togglebtn = document.querySelector('.toggle-btn')
-let navbar = document.querySelector('.navbar')
+let togglebtn = document.querySelector(".toggle-btn");
+let navbar = document.querySelector(".navbar");
 
-togglebtn.addEventListener('click', ()=>{
-    navbar.classList.toggle('mobile-nav')
-} )
+togglebtn.addEventListener("click", () => {
+  navbar.classList.toggle("mobile-nav");
+});
 
 function myFunction() {
-var element = document.body;
-element.classList.toggle("dark-mode");
+  var element = document.body;
+  element.classList.toggle("dark-mode");
 }
 
+let fname = document.getElementById("fname");
+let userform = document.getElementById("form-validate");
+let last = document.getElementById("lname");
 
+userform.addEventListener("submit", function validate(e) {
+  e.preventDefault();
+  localStorage.setItem("first name", JSON.stringify(fname.value));
+  localStorage.setItem("last name", JSON.stringify(last.value));
+});
 
-let fname= document.getElementById("fname");
-let userform=document.getElementById("form-validate");
-let last= document.getElementById("lname");
-
-userform.addEventListener("submit", function validate(e){
-    e.preventDefault();
-    localStorage.setItem("first name",JSON.stringify(fname.value));
-    localStorage.setItem("last name",JSON.stringify(last.value));
-})
+let scrollBtn = document.getElementById("scroll-up");
+document.addEventListener("scroll", function () {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+});
